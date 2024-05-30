65cfc2d95561f6247094e04c13428b8f0fc22d95
sengooooo <lesley023@naver.com>
2024-05-30 23:24:19 +0900

    fix : header update

--- yourside/src/Page/Header/components/Header.jsx
+++ yourside/src/Page/Header/components/Header.jsx
@@ -1,35 +1,71 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../css/Header.css';
 import logo from '../img/logo.svg';
 import login from '../img/login.svg';
 
 const Header = () => {
+
+  const HeaderStyle = {
+    display: "flex",
+    width: "100%",
+    height: "6.26rem",
+    backgroundColor: "white",
+    flexDirection: "row",
+    alignItems: "center", 
+    justifyContent: "center",
+  }
+
+  const MenuStyle = {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",  
+  }
+
+  const NavStyle = {
+    display: "flex",
+    alignItems: "center",
+    gap: "4.5rem",
+    marginLeft : "11.38rem",
+    marginRight:"11.38rem"
+  }
+
+  const BtnStyle = {
+    display: "flex",
+    alignItems: "center"
+  }
+
+  const LogoStyle = {
+    width: "8.125rem",
+    height: "2.5rem",
+    display: "flex",
+    alignItems: "center"
+  }
+
   return (
-    <div className="header">
-      <div className="header-menu">
-        <div className="logo">
+    <div className="header" style={HeaderStyle}>
+      <div className="header-menu" style={MenuStyle}>
+        <div className="logo" style={LogoStyle}>
           <Link to="/" className="logo">
             <img src={logo} alt="" className="logo" />
             <span />
           </Link>
         </div>
 
-        <div className="nav">
+        <div className="nav" style={NavStyle}>
           <Link to="/" className="link">
-            <div className="button">홈</div>
+            <div className="button" style={BtnStyle}>홈</div>
           </Link>
           <Link to="/Notation" className="link">
-            <div className="button">공지사항</div>
+            <div className="button" style={BtnStyle}>공지사항</div>
           </Link>
           <Link to="/ContractReview" className="link">
-            <div className="button">내 계약서 검토</div>
+            <div className="button" style={BtnStyle}>내 계약서 검토</div>
           </Link>
           <Link to="/MyWork" className="link">
-            <div className="button">내 근로 정리</div>
+            <div className="button" style={BtnStyle}>내 근로 정리</div>
           </Link>
           <Link to="/QuestionAndAnswer" className="link">
-            <div className="button">네편 현답</div>
+            <div className="button" style={BtnStyle}>네편 현답</div>
           </Link>
         </div>
 
