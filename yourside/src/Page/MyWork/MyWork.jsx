2e1b04978b153b1700382879a8ec4cab7d279477
Jun279 <33.beautifulboy@gmail.com>
2024-05-30 23:30:42 +0900

    update MyWork.jsx 1차

--- yourside/src/Page/MyWork/MyWork.jsx
+++ yourside/src/Page/MyWork/MyWork.jsx
@@ -1,19 +1,20 @@
 import { styled } from 'styled-components';
+import Header from '../Header/components/Header';
 
 const Wrapper = styled.div`
   background: #EFF6FE;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  width: 80vw;
+  margin: 0 auto;
 `
 
 const HeaderText = styled.p`
+  margin-top: 5rem;
   color: var(--system-color-system-black-900, #111);
   font-family: Pretendard;
-  font-size: 3rem;
+  font-size: 2rem;
   font-style: normal;
   font-weight: 600;
-  line-height: 4.25rem; /* 141.667% */
+  line-height: 3rem; /* 141.667% */
   letter-spacing: -0.075rem;
 
   & span {
@@ -21,10 +22,26 @@ const HeaderText = styled.p`
   }
 `
 
+const BannerContainer = styled.div`
+  margin-top: 3.5rem;
+  display: flex;
+  gap: 2vw;
+` 
+
+const BannerImg = styled.img`
+
+`
+
 export default function MyWork() {
   return (
-    <Wrapper>
-      <HeaderText>현재 <span>나의 근로 상황</span> <br /> 얼마나 알고 계시나요?</HeaderText>
-    </Wrapper>  
+    <>
+      <Header />
+      <Wrapper>
+        <HeaderText>현재 <span>나의 근로 상황</span> <br /> 얼마나 알고 계시나요?</HeaderText>
+        <BannerContainer>
+
+        </BannerContainer>
+      </Wrapper>  
+    </>
   )
 }
